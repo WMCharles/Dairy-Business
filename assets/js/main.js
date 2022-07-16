d22941b2ebd9fa2b6f497f6525afbe84093e3223
WMCharles <charles.wafula@student.moringaschool.com>
2022-07-16 11:53:27 +0300

    add objects containing data

---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+const shedProduction = {A:510, B:308, C:486, D:572}
+const leapYear = {January:31, February:29, March:31, April:30, May:31, June:30, July:31, August:31, September:30, October:31, November:30, December:31}
+
