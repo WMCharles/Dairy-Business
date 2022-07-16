24c92217b745b682b1f4291573093b0b94fe68b4
WMCharles <charles.wafula@student.moringaschool.com>
2022-07-16 22:48:34 +0300

    add function to accept user input

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,5 @@
-let shedProduction = {}
-
-shedProduction.A = Number(prompt("Shed A:"))
-shedProduction.B = Number(prompt("Shed B:"))
-shedProduction.C = Number(prompt("Shed C:"))
-shedProduction.D = Number(prompt("Shed D:"))
-
 const leapYear = {January:31, February:29, March:31, April:30, May:31, June:30, July:31, August:31, September:30, October:31, November:30, December:31}
+let shedProduction = {}
 
 let selling_price = 45;
 var dailyProduction = 0;
@@ -17,34 +11,42 @@ const id1 = document.getElementById('total')
 const id2 = document.getElementById('income')
 const id3 = document.getElementById('LeapYear')
 
-const btn1 = document.getElementById('totalProduction')
 
-btn1.addEventListener('click', totalProduction)
-btn1.addEventListener('click', incomeOverTime)
-btn1.addEventListener('click', incomeLeapYear)
+function report(){
 
+    shedProduction.A = parseInt(document.getElementById('first-a').value);
+    shedProduction.B = parseInt(document.getElementById('first-b').value);
+    shedProduction.C = parseInt(document.getElementById('first-c').value);
+    shedProduction.D = parseInt(document.getElementById('first-d').value);
 
-function totalProduction(){
-    for (const property in shedProduction){
-        let shedDiv = document.createElement('p')
+    function totalProduction(){
+        for (const property in shedProduction){
 
-        shedDiv.innerHTML = `Your production is shed ${property} ${shedProduction[property]} litres per day`
+            let shedDiv = document.createElement('p')
 
-        id1.appendChild(shedDiv)
+            shedDiv.innerHTML = `Your production is shed ${property} ${shedProduction[property]} litres per day`
+
+            id1.appendChild(shedDiv)
+
+            console.log(`Your production is shed ${property} ${shedProduction[property]} litres per day`)
+        }
+    
+        for (const x in shedProduction){
+            dailyProduction += shedProduction[x];
+        }
+        
+        let totalDiv = document.createElement('p')
+
+        totalDiv.innerHTML = `The total production is ${dailyProduction} litres per day`
+
+        id1.appendChild(totalDiv)
+
+        console.log(`The total production is ${dailyProduction} litres per day`)
     }
-
-    for (const x in shedProduction){
-        dailyProduction += shedProduction[x];
-    }
-
-    let totalDiv = document.createElement('p')
-
-    totalDiv.innerHTML = `The total production is ${dailyProduction} litres per day`
-
-    id1.appendChild(totalDiv)
-}
-
-function incomeOverTime(){
+    
+    // function to calculate income per week(per year)
+    
+    function incomeOverTime(){
 
         let weekDiv = document.createElement('p')
         weekDiv.innerHTML = `Your weekly income will be ${dailyProduction*week*selling_price}`
@@ -53,17 +55,28 @@ function incomeOverTime(){
         let monthDiv = document.createElement('p')
         monthDiv.innerHTML = `Your yearly income will be ${dailyProduction*year*selling_price}`
         id2.appendChild(monthDiv)
-}
 
-function incomeLeapYear(){
-    for(const month in leapYear){
-
-        let leapDiv = document.createElement('p')
-
-        leapDiv.innerHTML = `Your income for ${month} is ${leapYear[month]*selling_price*dailyProduction}`
-        id3.appendChild(leapDiv)
+        console.log(`Your weekly income will be ${dailyProduction*week*selling_price}`)
+        console.log(`Your yearly income will be ${dailyProduction*year*selling_price}`)
     }
+    
+    // function to calculate income per month in a leap year
+    
+    function incomeLeapYear(){
+        for(const month in leapYear){
+
+            let leapDiv = document.createElement('p')
+
+            leapDiv.innerHTML = `Your income for ${month} is ${leapYear[month]*selling_price*dailyProduction}`
+            id3.appendChild(leapDiv) 
+            
+            console.log(`Your income for ${month} is ${leapYear[month]*selling_price*dailyProduction}`)
+        }
+    }
+
+    totalProduction()
+    console.log("")
+    incomeOverTime(selling_price, time)
+    console.log("")
+    incomeLeapYear()
 }
-
-
-
