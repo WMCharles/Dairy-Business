53aa5bbd94d38b171a1aac8e503c909c53138eb4
WMCharles <charles.wafula@student.moringaschool.com>
2022-07-16 11:54:05 +0300

    add variables

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
 const shedProduction = {A:510, B:308, C:486, D:572}
 const leapYear = {January:31, February:29, March:31, April:30, May:31, June:30, July:31, August:31, September:30, October:31, November:30, December:31}
 
+let selling_price = 45;
+var dailyProduction = 0;
+let week = 7;
+let year = 365;
+let time;
+
